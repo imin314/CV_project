6f1c7d53b522738a2659db000019720a81072def
I Min <imin314@gmail.com>
2018-07-31 15:11:34 +0300

    setting webpack.config, adding layout for header section

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+      contentBase: './dist'
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './src/index.pug',
+    })
+    ],
+    module: {
+    rules: [
+        {
+            test: /\.pug$/,
+            loader: 'pug-loader',
+            options: {
+                pretty: true
+            }
+        },
+        {
+            test: /\.styl$/,
+            loader: 'style-loader!css-loader!stylus-loader'
+        }
+    ]
+    }
+};
