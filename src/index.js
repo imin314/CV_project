2517b187839c74591328cb66d6caf85eb2ed29d3
imin314 <imin314@gmail.com>
2019-09-25 09:51:14 +0700

    empty lines added before children items in stylus #6

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-import './assets/favicons/favicons.js'
+import './assets/favicons/favicons.js';
 import './styl/style.styl';
+/*
+require('./styl/style.styl');
+
+const context = require.context('./common.blocks', true, /\.(styl)$/);
+const files = {};
+
+context.keys().forEach((filename) => {
+  files[filename] = context(filename);
+});
+*/
